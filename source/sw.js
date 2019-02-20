f9f2f7467d71f0341d88815be8ebe1ed8ff3cfee
whyour <imwhyour@gmail.com>
2019-02-20 16:29:57 +0800

    update service worker

--- source/sw.js
+++ source/sw.js
@@ -1,12 +1,50 @@
-importScripts("./sw-toolbox.js"),
-	toolbox.router.get("/", self.toolbox.networkFirst, {
-		cache: {
-			name: "home-cache-v1",
-			maxEntries: 10
-		}
-	}), toolbox.router.get(/\.html$/, self.toolbox.networkFirst, {
-		cache: {
-			name: "page-cache-v1",
-			maxEntries: 200
-		}
-	});
+importScripts('./workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.html/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: 'html-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.css/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: 'css-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache image files
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  // Use the cache if it's available
+  workbox.strategies.cacheFirst({
+    // Use a custom cache name
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images
+        maxEntries: 20,
+        // Cache for a maximum of a week
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
